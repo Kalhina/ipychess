f9c7cb7d622b56abd2453a9e7a7f6dcc21406f90
uribalb <landrybete@gmail.com>
2020-05-17 16:34:33 +0000

    add board config to model and simplify rendering

--- src/widget.ts
+++ src/widget.ts
@@ -10,26 +10,35 @@ import {
 } from './version';
 
 import {Chessground} from 'chessground';
+// import { State, defaults } from 'chessground/state';
+
+import { Config} from 'chessground/config';
+
+
 
 // Import the CSS
 import '../css/widget.css'
 import '../css/chessground.css'
-// import '../css/board.css'
 import '../css/pieces.css'
 
-// import '../img/pieces/merida/wK.svg'
+class ConfigClass implements Config {}
+// const state = defaults() as Config;
+const config = new ConfigClass() as Config
+// configure(config, state)
 
 export
 class ExampleModel extends DOMWidgetModel {
   defaults() {
     return {...super.defaults(),
+      // config:configure(),,
       _model_name: ExampleModel.model_name,
       _model_module: ExampleModel.model_module,
       _model_module_version: ExampleModel.model_module_version,
       _view_name: ExampleModel.view_name,
       _view_module: ExampleModel.view_module,
       _view_module_version: ExampleModel.view_module_version,
-      value : 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+      options: [],
+      ...config
     };
   }
 
@@ -49,16 +58,17 @@ class ExampleModel extends DOMWidgetModel {
 
 export
 class ExampleView extends DOMWidgetView {
-  board_container: any; obj:any;
+  board_container: any; board:any;
   initialize() {
     // this.el.classList.remove('p-Widget');
     this.el.classList.add('brown', 'merida');
-    this.el.style.width = "500px"
-    this.el.style.height = "500px"
+    this.el.style.width = this.model.get('width')
+    this.el.style.height = this.model.get('width')
  
     this.board_container = document.createElement('div');
 
     this.el.appendChild(this.board_container)
+    this.board_container.classList.add("jupyter-widgets");
 
     document.addEventListener('scroll', (e)=>{window.requestAnimationFrame(()=>{document.body.dispatchEvent(new Event('chessground.resize')); console.log("scrolled!")})}, true);
     window.addEventListener('resize', (e)=>{window.requestAnimationFrame(()=>{document.body.dispatchEvent(new Event('chessground.resize')); console.log("resized!")})}, true);
@@ -67,47 +77,27 @@ class ExampleView extends DOMWidgetView {
   }
   render() {
     super.render();
-
-    // this.value_changed();
-    this.model.on('change:value', this.value_changed, this);
-
-    this.render_chessground();
-    // this.model.set('value', "rnbqkbnr/ppppp1pp/8/5p2/2P5/8/PP1PPPPP/RNBQKBNR w KQkq - 0 2")
+    this.model.on('change:fen', this.fen_changed, this);
+    setTimeout(this.render_chessground.bind(this), 0)
+    console.log(this.model.get('fen'))
 
 
   }
   render_chessground() {
-    // this.create_obj()
-     setTimeout(()=>{this.create_obj()}, 0)
-      // this.layer_views.update(this.model.get('layers'));
-      // this.control_views.update(this.model.get('controls'));
-      // this.leaflet_events();
-      // this.model_events();
-      // this.model.update_bounds().then(() => {
-      //   this.touch();
-      // });
+    let config:any = {
+      fen: this.model.get('fen'),
+      orientation: 'white'
+      }
+      
+    this.board = Chessground(this.board_container, config)
      
   }
-  create_obj() {
-     let config:any = {
-        fen: "",
-        orientation: 'white'
-        
-        }
-        
-      this.obj = Chessground(this.board_container, config)
-      
-      
-  }
-  value_changed() {
-    // this.board_container.textContent = this.model.get('value');
- 
+  fen_changed() {
+    // this.board_container.textContent = this.model.get('fen');
     let config:any = {
-      fen: this.model.get('value'),
+      fen: this.model.get('fen'),
       }
-    this.obj.set(config)
-    // this.obj = Chessground(this.board_container, config)
-   
+    this.board.set(config)   
   }
   update() {
      
