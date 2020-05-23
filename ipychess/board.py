b17142b0f036f19ecef200d1434d46f4e2b03bd7
uribalb <landrybete@gmail.com>
2020-05-23 15:35:34 +0000

    specify chessboard config types

--- ipychess/board.py
+++ ipychess/board.py
@@ -9,10 +9,10 @@ TODO: Add module docstring
 """
 
 from ipywidgets import DOMWidget
-from traitlets import Unicode, Union, List, Dict, Bool, Int, TraitError, default, validate
+from traitlets import Unicode, Union, List, Dict, Bool, default
 
 from ._frontend import module_name, module_version
-from .types import Color, Role, Key, File, Rank
+from .types import Color, Key
 
 
 class Board(DOMWidget):
@@ -28,18 +28,20 @@ class Board(DOMWidget):
     width = Unicode('400px').tag(sync=True)
 
     # Chessground options
-    fen = Unicode('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1').tag(sync=True, o=True)
+    fen = Unicode(
+        'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1').tag(sync=True, o=True)
     orientation = Color().tag(sync=True, o=True)
     turn_color = Color().tag(sync=True, o=True)
-    check = Union([Color(), Bool()], default_value=False).tag(sync=True, o=True)
-    last_move = Unicode().tag(sync=True, o=True)
-    selected = Unicode().tag(sync=True, o=True)
-    coordinates = Unicode().tag(sync=True, o=True)
-    auto_castle = Unicode().tag(sync=True, o=True)
-    view_only = Unicode().tag(sync=True, o=True)
-    disable_context_menu = Unicode().tag(sync=True, o=True)
+    check = Union([Color(), Bool()], default_value=False).tag(
+        sync=True, o=True)
+    last_move = List(trait=Key()).tag(sync=True, o=True)
+    selected = Key().tag(sync=True, o=True)
+    coordinates = Bool(default_value=True).tag(sync=True, o=True)
+    auto_castle = Bool(default_value=True).tag(sync=True, o=True)
+    view_only = Bool(default_value=False).tag(sync=True, o=True)
+    disable_context_menu = Bool(default_value=False).tag(sync=True, o=True)
     resizable = Bool(default_value=True).tag(sync=True, o=True)
-    add_piece_z_index = Unicode().tag(sync=True, o=True)
+    add_piece_z_index = Bool(default_value=False).tag(sync=True, o=True)
     highlight = Dict().tag(sync=True, o=True)
     animation = Dict().tag(sync=True, o=True)
     movable = Dict().tag(sync=True, o=True)
@@ -50,7 +52,7 @@ class Board(DOMWidget):
     events = Dict().tag(sync=True, o=True)
     drawable = Dict().tag(sync=True, o=True)
 
-    options = List(trait=Unicode()).tag(sync=True)
+    options = List(trait=Unicode()).tag(sync=True, o=False)
 
     @default('options')
     def _default_options(self):
